86b31b32300b2524664c30f0b12c0a7cd8a1c036
LanderDeRoeck <Lander.DeRoeck@student.uantwerpen.be>
2019-07-02 13:29:10 +0200

    Redo

--- source/Logic/HighSoreModule.cpp
+++ source/Logic/HighSoreModule.cpp
@@ -6,42 +6,44 @@
 #include <fstream>
 #include <iostream>
 
-roadfighter::HighSoreModule::HighSoreModule() {
-    std::string line;
-    std::ifstream myfile(savefile);
-    if (myfile.is_open())
-    {
-        // Name
-        getline(myfile, line);
-        getline(myfile, line);
+roadfighter::HighSoreModule::HighSoreModule()
+{
+        std::string line;
+        std::ifstream myfile(savefile);
+        if (myfile.is_open()) {
+                for (int i = 0; i < 4; i++) {
+                        // Scans variable name
+                        getline(myfile, line);
+                        if (line == "Highscore:") {
+                                getline(myfile, line);
+                                highscore = std::stoi(line);
+                                std::cout << highscore;
+                                continue;
+                        }
 
-        // Highscore
-        getline(myfile, line);
-        getline(myfile, line);
-        highscore = std::stoi(line);
-        std::cout << highscore;
+                        // Does nothing if variable doen't match
+                        getline(myfile, line);
+                }
 
-        myfile.close();
-    }
+                myfile.close();
+        }
 
-    else
-    {
-        std::ofstream outfile(savefile);
-        outfile << "Name:\n\nHighscore:\n0";
-        outfile.close();
-    }
+        else {
+                std::ofstream outfile(savefile);
+                outfile << "Name:\n\nHighscore:\n0";
+                outfile.close();
+        }
 }
 
-int roadfighter::HighSoreModule::getHighscore() const {
-    return highscore;
-}
+int roadfighter::HighSoreModule::getHighscore() const { return highscore; }
 
-void roadfighter::HighSoreModule::save(int score) {
-    if (score > highscore) {
-        std::ofstream outfile(savefile);
+void roadfighter::HighSoreModule::save(int score)
+{
+        if (score > highscore) {
+                std::ofstream outfile(savefile);
 
-        outfile << "Name:\n\nHighscore:\n" << score;
+                outfile << "Name:\n\nHighscore:\n" << score;
 
-        outfile.close();
-    }
+                outfile.close();
+        }
 }
