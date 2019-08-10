59c9bf0a973362af76a87d650b82564df8faeda9
LanderDeRoeck <Lander.DeRoeck@student.uantwerpen.be>
2019-08-10 13:32:09 +0200

    Changing scaling

--- source/sfmlgraph/PassingCarSFML.cpp
+++ source/sfmlgraph/PassingCarSFML.cpp
@@ -16,10 +16,12 @@ PassingCarSFML::PassingCarSFML(const std::shared_ptr<sf::RenderWindow>& window, 
                 texture.loadFromFile("./Resources/PassingCar.png");
         }
         sprite.setTexture(texture);
-        sprite.setScale(RoadFighter::Transformation::Instance()->getScaleX(),
-                        RoadFighter::Transformation::Instance()->getScaleY());
-        r_w = (texture.getSize().x * RoadFighter::Transformation::Instance()->getScaleX() * 4.0 / window->getSize().x);
-        r_h = (texture.getSize().y * RoadFighter::Transformation::Instance()->getScaleY() * 3.0 / window->getSize().y);
+        sprite.setScale(RoadFighter::Transformation::Instance()->getScaleX() / 2,
+                        RoadFighter::Transformation::Instance()->getScaleY() / 2);
+        r_w = (texture.getSize().x * RoadFighter::Transformation::Instance()->getScaleX() / 2 * 4.0 /
+               window->getSize().x);
+        r_h = (texture.getSize().y * RoadFighter::Transformation::Instance()->getScaleY() / 2 * 3.0 /
+               window->getSize().y);
         c_x = x;
         c_y = y;
 }
