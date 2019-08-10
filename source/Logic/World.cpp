33d3c1cc18acdd6cdcc2b5c21c931838f95b30f6
LanderDeRoeck <Lander.DeRoeck@student.uantwerpen.be>
2019-08-10 21:54:39 +0200

    Fixing hitboxes

--- source/Logic/World.cpp
+++ source/Logic/World.cpp
@@ -13,8 +13,8 @@ std::tuple<bool, bool> RoadFighter::World::entityCollision(const std::shared_ptr
                 return {false, false};
         bool left_x = ent1->getLeftX() >= ent2->getLeftX() && ent1->getLeftX() <= ent2->getRightX();
         bool right_x = ent1->getRightX() >= ent2->getLeftX() && ent1->getRightX() <= ent2->getRightX();
-        bool upper_y = ent1->getUpperY() <= ent2->getUpperY() && ent1->getUpperY() >= ent2->getLowerY();
-        bool lower_y = ent1->getLowerY() <= ent2->getUpperY() && ent1->getLowerY() >= ent2->getLowerY();
+        bool upper_y = ent1->getUpperY() >= ent2->getUpperY() && ent1->getUpperY() <= ent2->getLowerY();
+        bool lower_y = ent1->getLowerY() >= ent2->getUpperY() && ent1->getLowerY() <= ent2->getLowerY();
         return {left_x && (upper_y || lower_y), right_x && (upper_y || lower_y)};
 }
 
